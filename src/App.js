c993d2f74d71b74ad657fd56db9453567bf8a4d4
Pranjal <agrawalpranjal15@gmail.com>
2022-11-17 19:51:45 +0530

    initiated tailwind and daisyUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <h1>Hello world!</h1>
+    <input type="checkbox" class="toggle"/>
+    
+    </>
   )
 }
 
