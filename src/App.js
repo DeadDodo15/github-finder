a36ba54a2225118dda195f676f6668124c1310d6
Pranjal <agrawalpranjal15@gmail.com>
2022-11-27 13:59:18 +0530

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './Components/Layout/Navbar';
 
 function App() {
   return (
-    <>
-    <h1>Hello world!</h1>
-    <input type="checkbox" class="toggle"/>
-    
-    </>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <main>Content</main>
+      </div>
+    </Router>
   )
 }
 
